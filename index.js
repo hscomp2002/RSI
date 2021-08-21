0a817b541c46b7cb0dc3383e9e317850ad918db4
hscomp <hscomp2002@gmail.com>
2021-08-21 22:41:29 +0430

    .

--- index.js
+++ index.js
@@ -9,9 +9,30 @@ const binance = new Binance().options({
 var RSI = require('technicalindicators').RSI;
 
 
-function getRSI(symbole){
-	var inputRSI = {
-  values : [127.75,129.02,132.75,145.40,148.98,137.52,147.38,139.05,137.23,149.30,162.45,178.95,200.35,221.90,243.23,243.52,286.42,280.27,277.35,269.02,263.23,214.90],
-  period : 14
-};
+function getRSI(inputRSI) {
+  return RSI.calculate(inputRSI);
+}
+
+function getCandles(symbol) {
+  return new Promise((resolve, reject) => {
+    try {
+      binance.candlesticks(symbol, process.env.TIMEFRAME, (error, ticks, symbol) => {
+        let closePriceArray = [];
+        for (let i in ticks) {
+          if (i != ticks.length - 1) {
+            closePriceArray.push(Number(ticks[i][4]));
+          }
+        }
+        let rsi = getRSI({
+          values: closePriceArray,
+          period: 14
+        });
+        console.log({rsi});
+        resolve(rsi);
+      }, { limit: 15, endTime: +new Date() });
+    } catch (error) {
+      reject(error);
+    }
+
+  });
 }
